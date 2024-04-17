bc6e8276f0fa734e2e256fcb795d4b8f8e7daf8d
pehilbert <pehilbert03@gmail.com>
2024-04-16 22:58:37 -0700

    Add files to src

---
+++ src/Nexus.java
@@ -0,0 +1,14 @@
+public class Nexus
+{
+    public static void main( String[] args )
+    {
+        if (args.length != 2)
+        {
+            System.out.println("Usage: nexus <input filename> <target filename>");
+            System.exit(1);
+        }
+
+        System.out.println(args[0] + " compiled to " + args[1]);
+        System.exit(0);
+    }
+}
